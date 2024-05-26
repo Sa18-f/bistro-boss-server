f7bee0d5d546596d068ba10f3b72ce73fa304c45
Sa18-f <sisazid182713@gmail.com>
2024-05-26 23:45:17 +0600

    verify admin added

--- index.js
+++ index.js
@@ -32,44 +32,54 @@ async function run() {
         const menuCollection = client.db('bistroDB').collection('menu');
         const reviewCollection = client.db('bistroDB').collection('review');
         const cartsCollection = client.db('bistroDB').collection('carts');
-        
+
         // jwt related api
         app.post("/jwt", async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: "1h"
             });
-            res.send({token})
+            res.send({ token })
         })
         // middlewares
         const verifyToken = (req, res, next) => {
-            console.log("inside verify token", req.headers.authorization);
-            if(!req.headers.authorization){
-                return res.status(401).send({message: "forbidden access"})
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" })
             };
             const token = req.headers.authorization.split(" ")[1];
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if(err){
-                    return res.status(401).send({message: "forbidden access"})
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" })
                 }
                 req.decoded = decoded;
                 next()
             })
         }
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
 
         // check admin 
-        app.get("/user/admin/:email", verifyToken, async (req, res) => {
+        app.get("/users/admin/:email", verifyToken, async (req, res) => {
             const email = req.params.email;
-            if(email !== req.decoded.email){
-                return res.status(403).send({message: "unauthorized access"})
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "forbidden access" })
             }
-            const query = {email: email};
+            const query = { email: email };
             const user = await userCollection.findOne(query);
             let admin = false;
-            if(user){
+            if (user) {
                 admin = user?.role === 'admin'
             }
-            res.send({admin})
+            res.send({ admin })
         })
 
         app.get('/review', async (req, res) => {
@@ -83,7 +93,7 @@ async function run() {
         })
 
         // user related api 
-        app.get('/users',  verifyToken, async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
@@ -103,7 +113,7 @@ async function run() {
         })
 
         // admin update related api
-        app.patch("/users/admin/:id", async (req, res) => {
+        app.patch("/users/admin/:id",verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
@@ -115,7 +125,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete("/users/:id", async (req, res) => {
+        app.delete("/users/:id",verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);

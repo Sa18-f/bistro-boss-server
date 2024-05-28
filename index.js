d04d87ca1cd2b8db671e6bbf7bd1330b5bdd06da
Sa18-f <sisazid182713@gmail.com>
2024-05-28 19:59:25 +0600

    add item related post operation completed

--- index.js
+++ index.js
@@ -86,9 +86,21 @@ async function run() {
             const result = await reviewCollection.find().toArray();
             res.send(result)
         })
-
+        // menu related apis
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result)
+        });
+        app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+            const item = req.body;
+            const result =  await menuCollection.insertOne(item);
+            res.send(result)
+        })
+
+        app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
             res.send(result)
         })
 
